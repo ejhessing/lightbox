64521f077b7117acba42542127897af4bc1aa258
Erwin <ejhessing@gmail.com>
2016-05-09 11:16:19 +1200

    Added sudo code in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Need to create a lightbox - An overlay that displays the full size image when the thumbnail is clicked
+
+// Capture the Click event on a link to an image
+	// Show the Overlay
+	// Update the overlay with the image linked from the thumbnail
+	// Make the alt attribute the caption for the image 
+// add overlay
+	// add image and caption
+// When overlay is clicked
+	//hide overlay
+
